8fd6ab7b85cb7483d1d1f93904d8c6e7a04820be
tstrassb <tstrassb@student.42wolfsburg.de>
2023-09-08 13:03:06 +0200

    main modified

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tstrassb <tstrassb@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 12:49:48 by tstrassb          #+#    #+#             */
-/*   Updated: 2023/09/08 12:58:24 by tstrassb         ###   ########.fr       */
+/*   Updated: 2023/09/08 13:01:27 by tstrassb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 int main()
 {
     std::string str = "HI THIS IS BRAIN";
-    std::string&    stringREF   =   str;
     std::string *stringPTR  =   &str;
+    std::string&    stringREF   =   str;
 
     std::cout << "STR - address       : " << &str << std::endl;
-    std::cout << "stringREF - address : " << &stringREF << std::endl;
     std::cout << "stringPTR - address : " << &stringPTR << std::endl;
+    std::cout << "stringREF - address : " << &stringREF << std::endl;
     std::cout << "------------------------------------------" << std::endl;
     std::cout << "STR - value         : " << str << std::endl;
-    std::cout << "stringREF - value   : " << stringREF << std::endl;
     std::cout << "stringPTR - value   : " << *stringPTR << std::endl;
+    std::cout << "stringREF - value   : " << stringREF << std::endl;
 }
