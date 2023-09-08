f977214ef02ca4cf1a0bdf7fe02096f362885990
tstrassb <tstrassb@student.42wolfsburg.de>
2023-09-08 13:00:03 +0200

    ex02 added

---
+++ Module01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tstrassb <tstrassb@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 12:49:48 by tstrassb          #+#    #+#             */
+/*   Updated: 2023/09/08 12:58:24 by tstrassb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string&    stringREF   =   str;
+    std::string *stringPTR  =   &str;
+
+    std::cout << "STR - address       : " << &str << std::endl;
+    std::cout << "stringREF - address : " << &stringREF << std::endl;
+    std::cout << "stringPTR - address : " << &stringPTR << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "STR - value         : " << str << std::endl;
+    std::cout << "stringREF - value   : " << stringREF << std::endl;
+    std::cout << "stringPTR - value   : " << *stringPTR << std::endl;
+}
