2dfb0210b93eee9eee6eff54f5780a0cd4d77a8d
tstrassb <tstrassb@student.42wolfsburg.de>
2023-10-04 11:11:57 +0200

    restart ex00

--- Module03/ex00/ClapTrap.cpp
+++ Module03/ex00/ClapTrap.cpp
@@ -6,43 +6,9 @@
 /*   By: tstrassb <tstrassb@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:55:52 by tstrassb          #+#    #+#             */
-/*   Updated: 2023/10/03 12:07:37 by tstrassb         ###   ########.fr       */
+/*   Updated: 2023/10/04 11:11:27 by tstrassb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-//[**** Constructor ****]
-ClapTrap::ClapTrap() : _name(""), _hitPoint(10), _energyPoint(10), _attackDamage(0)
-{
-    std::cout << _name << " is created with " << "[Default constructor]" << std::endl;
-}
-
-ClapTrap::~ClapTrap()
-{
-    std::cout << _name << " is closed." << std::endl;
-}
-
-ClapTrap::ClapTrap(const ClapTrap &c)
-{
-    _name = c._name;
-    _hitPoint = c._hitPoint;
-    _energyPoint = c._energyPoint;
-    _attackDamage = c._attackDamage;
-    std::cout << _name << " is created with " << "[Copy constructor]" << std::endl;
-}
-
-ClapTrap::ClapTrap(std::string name) : _name(name), _hitPoint(10), _energyPoint(10), _attackDamage(0)
-{
-    std::cout << _name << " is created with " << "[String constructor]" << std::endl;
-}
-
-//[**** Public Members ****]
-void    ClapTrap::attack(const std::string& target)
-{
-    if (_energyPoint > 0)
-        std::cout << _name << " attacks to " << target << " and deal " << _attackDamage << " damage." << std::endl;
-    _energyPoint--;
-}
-
-void    ClapTrap::takeDamage(unsigned int amount)
--- Module03/ex00/main.cpp
+++ Module03/ex00/main.cpp
@@ -1,20 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tstrassb <tstrassb@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 11:50:41 by tstrassb          #+#    #+#             */
-/*   Updated: 2023/10/04 11:11:16 by tstrassb         ###   ########.fr       */
+/*   Created: 2023/10/04 10:53:16 by tstrassb          #+#    #+#             */
+/*   Updated: 2023/10/04 11:11:06 by tstrassb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
-
-#include <string>
-#include <iostream>
-
-
-#endif
+#include "ClapTrap.hpp"
