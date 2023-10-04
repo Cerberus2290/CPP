ae1fe04bc620cbb0619a299d3ac963746215909f
tstrassb <tstrassb@student.42wolfsburg.de>
2023-10-04 11:52:08 +0200

    M03e00 - new approach

--- Module03/ex00/ClapTrap.hpp
+++ Module03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: tstrassb <tstrassb@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:50:41 by tstrassb          #+#    #+#             */
-/*   Updated: 2023/10/04 11:11:16 by tstrassb         ###   ########.fr       */
+/*   Updated: 2023/10/04 11:27:13 by tstrassb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,29 @@
 
 #include <string>
 #include <iostream>
+#include <cstdlib>
 
+class   ClapTrap
+{
+    public:
+        // Constructors
+            ClapTrap();
+            ClapTrap(std::string name);
+            ClapTrap(const ClapTrap &claptrap);
+        // Overload Assignment Operator
+            ClapTrap& operator=(const ClapTrap &claptrap);
+        // Member Functions
+            void    attack(const std::string& target);
+            void    takeDamage(unsigned int amount);
+            void    beRepaired(unsigned int amount);
+        // Destructor
+            ~ClapTrap();
+    
+    private:
+        std::string _name;
+        size_t      _hitPoints;
+        size_t      _energyPoints;
+        size_t      _attackDamage;
+};
 
 #endif
