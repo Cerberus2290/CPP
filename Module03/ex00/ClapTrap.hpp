3278806b3039e28f5ce03e0769d7155e498f4141
tstrassb <tstrassb@student.42wolfsburg.de>
2023-11-08 06:43:00 +0000

    ex00 reworked

--- Module03/ex00/ClapTrap.hpp
+++ Module03/ex00/ClapTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tstrassb <tstrassb@student.42>             +#+  +:+       +#+        */
+/*   By: tstrassb <tstrassb@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:50:41 by tstrassb          #+#    #+#             */
-/*   Updated: 2023/10/04 11:27:13 by tstrassb         ###   ########.fr       */
+/*   Updated: 2023/11/08 06:35:36 by tstrassb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ class   ClapTrap
         // Constructors
             ClapTrap();
             ClapTrap(std::string name);
-            ClapTrap(const ClapTrap &claptrap);
+            ClapTrap(ClapTrap const &claptrap);
         // Overload Assignment Operator
-            ClapTrap& operator=(const ClapTrap &claptrap);
+            ClapTrap& operator=(ClapTrap const &claptrap);
         // Member Functions
             void    attack(const std::string& target);
             void    takeDamage(unsigned int amount);
@@ -35,9 +35,9 @@ class   ClapTrap
     
     private:
         std::string _name;
-        size_t      _hitPoints;
-        size_t      _energyPoints;
-        size_t      _attackDamage;
+        int         _hitPoints;
+        int         _energyPoints;
+        int         _attackDamage;
 };
 
 #endif
--- Module03/ex00/main.cpp
+++ Module03/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tstrassb <tstrassb@student.42>             +#+  +:+       +#+        */
+/*   By: tstrassb <tstrassb@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:53:16 by tstrassb          #+#    #+#             */
-/*   Updated: 2023/10/04 13:01:49 by tstrassb         ###   ########.fr       */
+/*   Updated: 2023/11/08 06:41:29 by tstrassb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,31 @@
 
 int main( void )
 {
-    ClapTrap    bot("Box");
+    ClapTrap    defaultBox;
+    defaultBox.attack("Dummy Trap");
+    defaultBox.takeDamage(30);
+    defaultBox.beRepaired(10);
+    defaultBox.attack("Dummy Trap");
+    
+    ClapTrap    Box2("Box2");
+    Box2.attack("Dummy Trap 2");
+    Box2.takeDamage(9);
+    Box2.beRepaired(10);
+    Box2.attack("Dummy Trap 2");
 
-    bot.attack("Claptrap");
-    bot.attack("Claptrap");
-    bot.attack("Claptrap");
-    bot.attack("Claptrap");
-    bot.attack("Claptrap");
-    bot.attack("Claptrap");
-    bot.attack("Claptrap");
-    bot.takeDamage(8);
-    bot.attack("Claptrap");
-    bot.beRepaired(5);
-    bot.beRepaired(5);
-    bot.beRepaired(5);
-    bot.takeDamage(18);
-    bot.takeDamage(20);
-    bot.beRepaired(5);
-    bot.beRepaired(5);
-    bot.attack("void");
+    ClapTrap    Box2_duplicate(Box2);
+    Box2_duplicate.attack("Dummy Trap 3");
+    Box2_duplicate.takeDamage(10);
+    Box2_duplicate.takeDamage(9);
+    Box2_duplicate.takeDamage(9);
+    Box2_duplicate.beRepaired(15);
+    Box2_duplicate.attack("Dummy Trap 3");
+    Box2_duplicate.beRepaired(15);
+    Box2_duplicate.beRepaired(15);
+    Box2_duplicate.beRepaired(15);
+    Box2_duplicate.beRepaired(15);
+    Box2_duplicate.beRepaired(15);
+    Box2_duplicate.attack("Dummy Trap 3");
 
     return 0;
 }
